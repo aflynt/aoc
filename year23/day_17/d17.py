66c9de468d567399416447bea5b4ae73836e7d8a
austin flynt <aflynt1@gmail.com>
2023-12-18 11:21:29 -0600

    day17 works on example

--- year23/day_17/d17.py
+++ year23/day_17/d17.py
@@ -1,7 +1,8 @@
 from lib import *
 from queue import PriorityQueue
 
-fname = "ex.txt"
+#fname = "ex.txt"
+fname = "in.txt"
 G = get_grid(fname)
 R = len(G)
 C = len(G[0])
@@ -10,35 +11,62 @@ C = len(G[0])
 
 # A* finds a path from start to goal.
 # h is the heuristic function. h(n) estimates the cost to reach goal from node n
-def A_Star(start, goal, h):
+def A_Star(start: Node, goal: Node):
     # The set of discovered nodes that may need to be (re-)expanded.
     # Initially, only the start node is known.
     # This is usually implemented as a min-heap or pqueue
-    openSet = PriorityQueue()
-    openSet.put(start)
+    openSet = []
+    #openSet = PriorityQueue()
+    #openSet.put((0,start)) # TODO: do i chg to (fscore, node)? how does it get sorted?
 
     # empty map of cameFrom[n] -> node before n
     cameFrom = {}
 
     # For node n, gScore[n] is cost of cheapest path from start to n currently known.
-    #gScore = init_gscore(start, R, C)
-    gScore = {} # defaults = infinity??
+    gScore = init_gscore( R, C)
     gScore[start] = 0
 
     # For node n, fScore[n]: gScore[n] + h(n). fScore[n] represents our current best guess as to
     # how cheap a path could be from start to finish if it goes through n.
-    #fScore = init_fscore(start, R, C)
-    fScore = {} # defaults = infinity??
-    fScore[start] = h(start, R, C)
+    fScore = init_fscore( R, C)
+    fScore[start] = h(start, goal)
+    #openSet.put((fScore[start], start))
+    openSet.append((fScore[start], start))
 
-    while not openSet.empty():
-        cn = openSet.get() # get current node
-        cr = cn[1]
-        cc = cn[2]
-        if (cr,cc) == (R-1, C-1):
+    i = 0
+
+    #while not openSet.empty():
+    while len(openSet) > 0:
+        i += 1
+        print(i)
+        if i > 12000:
+            break
+
+        #_, cn = openSet.get() # get current node
+        openSet = sorted(openSet, reverse=True)
+        _, cn = openSet.pop() # get current node
+
+        if (cn.r,cn.c) == (goal.r, goal.c):
             return reconstruct_path(cameFrom, cn)
 
-        nbrs = get_nbrs(G, cn)
+        nbrs = get_node_nbrs(G, cn)
+
+        for nbr in nbrs:
+            #key_nbr = (nbr.r, nbr.c, nbr.mvs, nbr.dir)
+            # d(cn,nbr) = weight of edge from current to neighbor
+            # tentative gScore is distance from start to nbr thru current node
+            tentative_gScore = gScore[cn] + d(nbr, G)
+
+            if tentative_gScore < gScore[nbr]:
+                # this path to nbr is better than any previous one. Record it!
+                cameFrom[nbr] = cn
+                gScore[nbr] = tentative_gScore
+                fScore[nbr] = tentative_gScore + h(nbr, goal)
+                f = fScore[nbr]
+                if (f,nbr) not in openSet:
+                    #openSet.put((f,nbr))
+                    openSet.append((f,nbr))
+                
         
 
 
@@ -48,25 +76,38 @@ def A_Star(start, goal, h):
 ################################################
 
 #      f, r, c, mvs, dir 
-pos = (0, 0, 0, 0, Dir.E)
+#epos = (0, 0, 0, 0, Dir.E)
+sn = Node(0,0,0,Dir.E)
+gn = Node(R-1,C-1,0,Dir.N)
+nodes = A_Star(sn, gn)
 
 ans = 0
-val = G[0][0]
-r = 0
-c = 0
-
-while True:
-    print(f"now at: ({r},{c}) val: {val}, ans: {ans}")
-    nbrs = get_nbrs(G, pos)
-    for i,nbr in enumerate(nbrs):
-        print(f"{i:2d} {nbr}")
-    pic = input("pick nbr #: ")
-    pic = int(pic)
-    pos = nbrs[pic]
-    print(f" -> new_pos = {pos}")
-    r = pos[1]
-    c = pos[2]
-    val = G[r][c]
+for node in nodes:
+    val = 0
+    if node != sn:
+        val = d(node, G)
+    
     ans += val
+    print(f"val: {val:3d} sum: {ans:3d} node: {node}")
+
+#
+##ans = 0
+#val = G[0][0]
+#r = 0
+#c = 0
+
+#while True:
+#    print(f"now at: ({r},{c}) val: {val}, ans: {ans}")
+#    nbrs = get_nbrs(G, pos)
+#    for i,nbr in enumerate(nbrs):
+#        print(f"{i:2d} {nbr}")
+#    pic = input("pick nbr #: ")
+#    pic = int(pic)
+#    pos = nbrs[pic]
+#    print(f" -> new_pos = {pos}")
+#    r = pos[1]
+#    c = pos[2]
+#    val = G[r][c]
+#    ans += val
 
 
--- year23/day_17/lib.py
+++ year23/day_17/lib.py
@@ -3,14 +3,83 @@ import sys
 import math as m
 
 class Dir(Enum):
-    N = "N"
-    S = "S"
-    E = "E"
-    W = "W"
+    N = 0
+    S = 1
+    E = 2
+    W = 3
     def __str__(self) -> str:
         return self.name
     def __repr__(self) -> str:
         return self.name
+    def __lt__(self, o):
+        return self.value < o.value
+
+class Node:
+    def __init__(self, r, c, mvs, dir):
+        self.r = r
+        self.c = c
+        self.mvs = mvs
+        self.dir = dir
+    def __lt__(self, o):
+        r = self.r
+        c = self.c
+        m = self.mvs
+        d = self.dir
+        _r = o.r
+        _c = o.c
+        _m = o.mvs
+        _d = o.dir
+        return ((r,c,m,d) < ((_r,_c,_m,_d)))
+    def __gt__(self, o):
+        r = self.r
+        c = self.c
+        m = self.mvs
+        d = self.dir
+        _r = o.r
+        _c = o.c
+        _m = o.mvs
+        _d = o.dir
+        return ((r,c,m,d) > ((_r,_c,_m,_d)))
+    def __le__(self, o):
+        r = self.r
+        c = self.c
+        m = self.mvs
+        d = self.dir
+        _r = o.r
+        _c = o.c
+        _m = o.mvs
+        _d = o.dir
+        return ((r,c,m,d) <= ((_r,_c,_m,_d)))
+    def __ge__(self, o):
+        r = self.r
+        c = self.c
+        m = self.mvs
+        d = self.dir
+        _r = o.r
+        _c = o.c
+        _m = o.mvs
+        _d = o.dir
+        return ((r,c,m,d) >= ((_r,_c,_m,_d)))
+    def __eq__(self, o):
+        r = self.r
+        c = self.c
+        m = self.mvs
+        d = self.dir
+        _r = o.r
+        _c = o.c
+        _m = o.mvs
+        _d = o.dir
+        return ((r,c,m,d) == ((_r,_c,_m,_d)))
+    def __hash__(self) -> int:
+        r = self.r
+        c = self.c
+        m = self.mvs
+        d = self.dir
+        return hash((r,c,m,d))
+    def __repr__(self) -> str:
+        return f"Node({self.r},{self.c},{self.mvs},{self.dir})"
+    def __str__(self) -> str:
+        return f"({self.r},{self.c},{self.mvs},{self.dir})"
 
 def get_grid(fname):
     with open(fname, "r") as f:
@@ -31,80 +100,85 @@ def pick_mv():
     x = input("dir: (0N, 1S, 2E, 3W)")
     return int(x)
 
-def get_nbrs(G, pos: tuple[int,int,int,int,Dir]) -> list[tuple[int,int,int,int,Dir]]:
+def get_node_nbrs(G, cn: Node) -> list[Node]:
     R   = len(G)
     C   = len(G[0])
-    f   = pos[0]
-    r   = pos[1]
-    c   = pos[2]
-    mvs = pos[3]
-    dir = pos[4]
+    r = cn.r
+    c = cn.c
+    mvs = cn.mvs
+    dir = cn.dir
     nbrs = []
     if dir == Dir.E:
         if 0 <= r-1 < R and 0 <= c   < C:
-            nbrs.append((0, r-1, c, 1, Dir.N)) 
+            nbrs.append(Node(r-1, c  , 1, Dir.N)) 
         if 0 <= r+1 < R and 0 <= c   < C:
-            nbrs.append((0, r+1, c, 1, Dir.S)) 
+            nbrs.append(Node(r+1, c  , 1, Dir.S)) 
         if 0 <= r   < R and 0 <= c+1 < C and mvs < 3:
-            nbrs.append((0, r, c+1, mvs+1, Dir.E)) 
+            nbrs.append(Node(r  , c+1, mvs+1, Dir.E)) 
     elif dir == Dir.W:
         if 0 <= r-1 < R and 0 <= c   < C:
-            nbrs.append((0, r-1, c, 1, Dir.N)) 
+            nbrs.append(Node( r-1, c, 1, Dir.N)) 
         if 0 <= r+1 < R and 0 <= c   < C:
-            nbrs.append((0, r+1, c, 1, Dir.S)) 
+            nbrs.append(Node( r+1, c, 1, Dir.S)) 
         if 0 <= r   < R and 0 <= c-1 < C and mvs < 3:
-            nbrs.append((0, r, c-1, mvs+1, Dir.W)) 
+            nbrs.append(Node( r, c-1, mvs+1, Dir.W)) 
     elif dir == Dir.N:
         if 0 <= r   < R and 0 <= c+1 < C:
-            nbrs.append((0, r, c+1, 1, Dir.E)) 
+            nbrs.append(Node( r, c+1, 1, Dir.E)) 
         if 0 <= r   < R and 0 <= c-1 < C:
-            nbrs.append((0, r, c-1, 1, Dir.W)) 
+            nbrs.append(Node( r, c-1, 1, Dir.W)) 
         if 0 <= r-1 < R and 0 <= c   < C and mvs < 3:
-            nbrs.append((0, r-1, c, mvs+1, Dir.N)) 
+            nbrs.append(Node( r-1, c, mvs+1, Dir.N)) 
     elif dir == Dir.S:
         if 0 <= r   < R and 0 <= c+1 < C:
-            nbrs.append((0, r, c+1, 1, Dir.E)) 
+            nbrs.append(Node( r, c+1, 1, Dir.E)) 
         if 0 <= r   < R and 0 <= c-1 < C:
-            nbrs.append((0, r, c-1, 1, Dir.W)) 
+            nbrs.append(Node( r, c-1, 1, Dir.W)) 
         if 0 <= r+1 < R and 0 <= c   < C and mvs < 3:
-            nbrs.append((0, r+1, c, mvs+1, Dir.S)) 
+            nbrs.append(Node( r+1, c, mvs+1, Dir.S)) 
     else:
         assert False
     return nbrs
 
-def reconstruct_path(cameFrom: dict[tuple[int,int,int,int,Dir]], current):
+def reconstruct_path(cameFrom: dict[Node], current: Node):
     total_path = [current]
     while current in cameFrom.keys():
         current = cameFrom[current]
         total_path.append(current)
     return total_path
 
-def h(node,R,C):
+def h(node: Node, goal: Node):
     # node = (f,r,c,mvs, dir)
-    r = node[1]
-    c = node[2]
-    gr = R-1
-    gc = C-1
+    r = node.r
+    c = node.c
+    gr = goal.r
+    gc = goal.c
     dx = abs(r-gr) + abs(c-gc)
     return dx
 
 
-def init_gscore(start,R,C):
+def init_gscore(R,C):
     gScore = {} # defaults = infinity??
     for r in range(R):
         for c in range(C):
             for mv in [1, 2, 3]:
                 for dir in [Dir.N, Dir.S, Dir.E, Dir.W]:
-                    gScore[(r,c,mv, dir)] = sys.maxsize
-    gScore[start] = 0
+                    n = Node(r,c,mv,dir)
+                    gScore[n] = sys.maxsize
     return gScore
 
-def init_fscore(start,R,C):
+def init_fscore(R,C):
     fScore = {} # defaults = infinity??
     for r in range(R):
         for c in range(C):
             for mv in [1, 2, 3]:
                 for dir in [Dir.N, Dir.S, Dir.E, Dir.W]:
-                    fScore[(r,c,mv, dir)] = sys.maxsize
-    fScore[start] = h(start, R, C)
+                    n = Node(r,c,mv,dir)
+                    fScore[n] = sys.maxsize
     return fScore
+
+def d(nbr: Node, G):
+    # weight of edge from current to neighbor
+    # cost is just cost of entering this node
+    val = G[nbr.r][nbr.c]
+    return val
