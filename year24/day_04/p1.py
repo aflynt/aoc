34dec5b24d369ed1746a3464b428b1d7429605f9
Austin Flynt <aflynt1@gmail.com>
2024-12-06 08:47:39 -0600

    day4 p1 works

--- year24/day_04/p1.py
+++ year24/day_04/p1.py
@@ -19,6 +19,17 @@ class Dir(Enum):
     def __lt__(self, o):
         return self.value < o.value
 
+dirs = [
+    Dir.E,
+    Dir.W,
+    Dir.N,
+    Dir.S,
+    Dir.SE,
+    Dir.SW,
+    Dir.NW,
+    Dir.NE,
+]
+
 class Node:
     def __init__(self, r, c, v):
         self.r = r
@@ -32,7 +43,7 @@ class Node:
     def __repr__(self) -> str:
         return f"Node({self.r:2d},{self.c:2d}, {self.v})"
     def __str__(self) -> str:
-        return f"({self.r:2d},{self.c:2d}, {self.v})"
+        return f"({self.r:3d},{self.c:3d}, {self.v})"
 
 def get_node_nbrs(G, cn: Node) -> list[Node]:
     R   = len(G)
@@ -42,7 +53,6 @@ def get_node_nbrs(G, cn: Node) -> list[Node]:
     v = cn.v
     nbrs = []
     # look east
-
 
 
     # try to add N
@@ -79,6 +89,61 @@ def get_node_nbrs(G, cn: Node) -> list[Node]:
 
     return nbrs
 
+def get_nbrs_matching_char(G, cn: Node, c: str):
+
+    mnbrs = []
+    for d in dirs:
+        nbr = get_nbr(G, cn, d)
+        if nbr and nbr.v == c:
+            mnbrs.append((d,nbr))
+    return mnbrs
+
+
+def get_nbr(G, cn: Node, dir: Dir):
+
+    R   = len(G)
+    C   = len(G[0])
+    r = cn.r
+    c = cn.c
+
+    match dir:
+        case Dir.N:
+            # try to add N
+            if 0 <= r-1 < R and 0 <= c   < C : 
+                return Node(r-1, c  , G[r-1][c])
+        case Dir.S:
+            # try to add south
+            if 0 <= r+1 < R and 0 <= c   < C :
+                return Node(r+1, c  , G[r+1][c])
+        case Dir.E:
+            # try to add east
+            if 0 <= r   < R and 0 <= c+1 < C :
+                return Node(r  , c+1, G[r][c+1])
+        case Dir.W:
+            # try to add west
+            if 0 <= r   < R and 0 <= c-1 < C :
+                return Node( r, c-1, G[r][c-1])
+        case Dir.NE:
+            # try to add NE
+            if 0 <= r-1   < R and 0 <= c+1 < C :
+                return Node( r-1, c+1, G[r-1][c+1])
+        case Dir.NW:
+            # NW = - -
+            if 0 <= r-1   < R and 0 <= c-1 < C :
+                return Node( r-1, c-1, G[r-1][c-1])
+        case Dir.SW:
+            # SW = + -
+            if 0 <= r+1   < R and 0 <= c-1 < C :
+                return Node( r+1, c-1, G[r+1][c-1])
+        case Dir.SE:
+            # SE = + +
+            if 0 <= r+1   < R and 0 <= c+1 < C :
+                return Node( r+1, c+1, G[r+1][c+1])
+        case _:
+            return None
+    return None
+
+
 def print_nums(mem):
     # print a list of numbers 
     for i in range(0,80):
@@ -108,45 +173,40 @@ def print_grid(G):
         nums = [str(G[r][c]) for c in range(C)]
         print("".join(nums))
 
-fname = "ex.txt"
+#fname = "ex.txt"
+fname = "in.txt"
 
 G = get_grid(fname)
 
 
-def get_xnodes(G):
-    xnodes = []
+
+def p1(G):
+    ans = 0
     R = len(G)
     C = len(G[0])
 
-    pstr = "."
     for r in range(R):
         for c in range(C):
             n = Node(r,c, G[r][c])
             if n.v == "X":
-                nbrs = get_node_nbrs(G, n)
-                mnbrs = [n for n in nbrs if n.v == "M"]
+                # ok found n is an x, now look for m
+                mnbrs = get_nbrs_matching_char(G, n, "M")
 
-                MEs = [n for n in mnbrs if n.r == r and n.c == c+1]
-                MWs = [n for n in mnbrs if n.r == r and n.c == c-1]
-                MNs = [n for n in mnbrs if n.r == r-1 and n.c == c]
-                MSs = [n for n in mnbrs if n.r == r+1 and n.c == c]
-                MSEs = [n for n in mnbrs if n.r == r+1 == n.c == c+1]
-                MSWs = [n for n in mnbrs if n.r == r+1 == n.c == c-1]
-                MNWs = [n for n in mnbrs if n.r == r-1 == n.c == c-1]
-                MNEs = [n for n in mnbrs if n.r == r-1 == n.c == c+1]
+                for mnbr in mnbrs:
+                    mdir,mn = mnbr
+                    # ok i have an Mnode in the direction of mdir
 
-                print(f"X: {n}")
-                print(f"  - MEs : {MEs }")
-                print(f"  - MWs : {MWs }")
-                print(f"  - MNs : {MNs }")
-                print(f"  - MSs : {MSs }")
-                print(f"  - MSEs: {MSEs}")
-                print(f"  - MSWs: {MSWs}")
-                print(f"  - MNWs: {MNWs}")
-                print(f"  - MNEs: {MNEs}")
+                    # get A in mdir direction
+                    anbr = get_nbr(G, mn, mdir)
+                    if anbr and anbr.v == 'A':
+                       snbr = get_nbr(G, anbr, mdir)
+                       if snbr and snbr.v == 'S':
+                           ans += 1
+                           print(f" {ans:2d}: {n}->{mdir:2s} {mn}, {anbr}, {snbr}")
 
 
 
-get_xnodes(G)
 
-print_grid(G)
+p1(G)
+
+#print_grid(G)
