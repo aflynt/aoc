1d2a32fc35a30cd7eb451ceab91cd4d23ac9800e
austin flynt <aflynt1@gmail.com>
2024-12-31 23:27:03 -0600

    day15 part 2

--- year24/day15.py
+++ year24/day15.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from time import sleep
 
 class Dir(Enum):
     def __init__(self) -> None:
@@ -38,12 +39,23 @@ class Grid:
         self.C = len(G[0])
         self.boxes = set()
         self.rings = set()
+        self.lrings = set()
+        self.rrings = set()
         self.xpos = (0,0)
         self.find_xpos()
         self.clear_rings()
         self.clear_xpos()
     def at(self,r,c):
-        return self.G[r][c]
+        v = self.G[r][c]
+        if (r,c) in self.rings:
+            v = "O"
+        elif (r,c) in self.rrings:
+            v = "]"
+        elif (r,c) in self.lrings:
+            v = "["
+        elif (r,c) == self.xpos:
+            v = "@"
+        return v
     def find_xpos(self):
         for r in range(self.R):
             for c in range(self.C):
@@ -54,8 +66,15 @@ class Grid:
                     self.boxes.add((r,c))
                 elif v == "O":
                     self.rings.add((r,c))
+                elif v == "[":
+                    self.lrings.add((r,c))
+                elif v == "]":
+                    self.rrings.add((r,c))
     def clear_rings(self):
-        for ring in self.rings:
+        for ring in self.rrings:
+            r,c = ring
+            self.G[r][c] = "."
+        for ring in self.lrings:
             r,c = ring
             self.G[r][c] = "."
     def clear_xpos(self):
@@ -67,174 +86,154 @@ class Grid:
                 v = self.G[r][c]
                 if (r,c) in self.rings:
                     v = "O"
+                elif (r,c) in self.rrings:
+                    v = "]"
+                elif (r,c) in self.lrings:
+                    v = "["
                 elif (r,c) == self.xpos:
                     v = "@"
-                print(v,end=" ")
+                #print(v,end=" ")
+                print(v,end="")
             print()
+
+    def shift_rings(self, dr,dc, mvs):
+        # get LR target mv lrings, rrings
+        slrings = mvs&self.lrings
+        srrings = mvs&self.rrings
+
+        # remove shifted rings
+        self.lrings -= slrings
+        self.rrings -= srrings
+
+        # shift rings
+        slrings = [(r+dr,c+dc) for r,c in slrings]
+        srrings = [(r+dr,c+dc) for r,c in srrings]
+
+        # add rings back
+        self.lrings |= set(slrings)
+        self.rrings |= set(srrings)
+
+        # move xpos
+        xr,xc = self.xpos
+        self.xpos = (xr+dr, xc+dc)
+
+    def can_mv(self, src, dest):
+        er = dest[0]
+        ec = dest[1]
+        sr = src[0]
+        sc = src[1]
+        dr = er - sr
+        dc = ec - sc
+        #dir = "E"
+        #if   dr < 0: dir = "N"
+        #elif dr > 0: dir = "S"
+        #elif dc < 0: dir = "W"
+        destval = self.at(er,ec)
+
+        dest_empty = dest not in self.lrings \
+                 and dest not in self.rrings \
+                 and dest not in self.boxes
+
+        if dest_empty:
+            return True
+        elif dest in self.boxes:
+            return False
+        elif destval == "[" and dc == 0: # N/S
+            # need to move both sides of ring
+            can_mv_L = self.can_mv((er,ec  ), (er+dr, ec  ))
+            can_mv_R = self.can_mv((er,ec+1), (er+dr, ec+1))
+            return can_mv_L and can_mv_R
+        elif destval == "]" and dc == 0: # N/S
+            can_mv_L = self.can_mv((er,ec  ), (er+dr, ec  ))
+            can_mv_R = self.can_mv((er,ec-1), (er+dr, ec-1))
+            return can_mv_L and can_mv_R
+        elif destval == "[" or destval == "]":
+            can_mv_LR = self.can_mv((sr,ec+dc),(sr,ec+2*dc))
+            return can_mv_LR
+        else:
+            assert False, "bad case of destval..."
+
+    def collect_moves(self, src, dest, mvs=set()):
+        er = dest[0]
+        ec = dest[1]
+        sr = src[0]
+        sc = src[1]
+        dr = er - sr
+        dc = ec - sc
+        destval = self.at(er,ec)
+        mvs.add((sr,sc))
+
+        dest_empty = dest not in self.lrings \
+                 and dest not in self.rrings \
+                 and dest not in self.boxes
+
+        if dest_empty:
+            mvs.add((sr,sc))
+            return mvs
+        elif destval == "[" and dc == 0: # N/S
+            # need to move both sides of ring
+            mvs.add((er,ec))
+            mvs.add((er,ec+1))
+            mvs_L = self.collect_moves((er,ec  ), (er+dr, ec  ), mvs)
+            mvs_R = self.collect_moves((er,ec+1), (er+dr, ec+1), mvs)
+            mvs |= mvs_L | mvs_R
+            return mvs
+        elif destval == "]" and dc == 0: # N/S
+            mvs.add((er,ec))
+            mvs.add((er,ec-1))
+            mvs_L = self.collect_moves((er,ec  ), (er+dr, ec  ), mvs)
+            mvs_R = self.collect_moves((er,ec-1), (er+dr, ec-1), mvs)
+            mvs |= mvs_L | mvs_R
+            return mvs
+        elif destval == "[" or destval == "]":
+            # se
+            # @[].
+            mvs.add((sr,sc))
+            mvs.add((sr,ec))
+            mvs.add((sr,ec+dc))
+            mvs_LR = self.collect_moves((sr,ec+dc),(sr,ec+2*dc), mvs)
+            mvs |= mvs_LR
+            return mvs
+        else:
+            assert False, "bad case of destval..."
+
+
     def mv_n(self):
         xr,xc = self.xpos
-        r0 = 0
-        r1 = xr-1
-        # find first rock == r0
-        rbox = r1
-        while rbox >= 0:
-            if (rbox, xc) in self.boxes:
-                break
-            rbox -= 1
-        r0 = rbox
-        rs = range(r1,r0, -1)
-        ci = xc
-        e = -1
-        for ri in rs:
-            # find open slot
-            is_box  = (ri,ci) in self.boxes
-            is_ring = (ri,ci) in self.rings
-            if not is_box and not is_ring:
-                # appears to be open
-                # move here
-                e = ri
-                break
 
-        # can move
-        if e > -1:
-            # shift rings in the way
-            #       e<-s
-            # PRE   .OO@
-            # POST  OO@
-            # shifting north
-            srs = set([(r,xc) for r in range(e,xr)])
-            srings = srs&self.rings
-            # isolated shifted rings
-            self.rings -= srings
-            # now shift up srings
-            srings = [(r-1,c) for r,c in srings]
-            # now add back to rings
-            self.rings |= set(srings)
-            # now move xpos
-            self.xpos = (xr-1, xc)
+        can_mv = self.can_mv((xr,xc), (xr-1,xc))
+        if can_mv:
+            mvs = set()
+            mvs = self.collect_moves((xr,xc), (xr-1,xc), mvs)
+            #print(f"CAN MV? {can_mv}, mvs = {mvs}")
+            self.shift_rings(-1, 0, mvs)
+
     def mv_s(self):
         xr,xc = self.xpos
-        r0 = xr+1   # first row is me + 1
-        r1 = self.R # last row is bottom
-        # find first rock between me and bottom
-        rbox = r0
-        while rbox < self.R:
-            if (rbox, xc) in self.boxes:
-                break
-            rbox += 1
-        r1 = rbox
-        rs = range(r0,r1+1)
-        ci = xc
-        e = -1
-        for ri in rs:
-            # find open slot
-            is_box  = (ri,ci) in self.boxes
-            is_ring = (ri,ci) in self.rings
-            if not is_box and not is_ring:
-                # appears to be open
-                # move here
-                e = ri
-                break
+        can_mv = self.can_mv((xr,xc), (xr+1,xc))
+        if can_mv:
+            mvs = set()
+            mvs = self.collect_moves((xr,xc), (xr+1,xc), mvs)
+            #print(f"CAN MV? {can_mv}, mvs = {mvs}")
+            self.shift_rings(1,0, mvs)
 
-        # can move
-        if e > -1:
-            # shift rings in the way
-            #       s_>e
-            # PRE   @OO.
-            # POST  .@OO
-            # shifting north
-            srs = set([(r,xc) for r in range(r0, e+1)])
-            srings = srs&self.rings
-            # isolated shifted rings
-            self.rings -= srings
-            # now shift up srings
-            srings = [(r+1,c) for r,c in srings]
-            # now add back to rings
-            self.rings |= set(srings)
-            # now move xpos
-            self.xpos = (xr+1, xc)
     def mv_e(self):
         xr,xc = self.xpos
-        c0 = xc+1    # c0 == start  search col
-        c1 = self.C  # c1 == ending search col
-        # find first rock between me and bottom
-        cbox = c0
-        while cbox < self.C:
-            if (xr,cbox) in self.boxes:
-                break
-            cbox += 1
-        c1 = cbox
-        cs = range(c0,c1)
-        ri = xr
-        e = -1
-        for ci in cs:
-            # find open slot
-            is_box  = (ri,ci) in self.boxes
-            is_ring = (ri,ci) in self.rings
-            if not is_box and not is_ring:
-                # appears to be open
-                # move here
-                e = ci
-                break
+        can_mv = self.can_mv((xr,xc),(xr,xc+1))
+        if can_mv:
+            mvs = set()
+            mvs = self.collect_moves((xr,xc), (xr,xc+1), mvs)
+            #print(f"CAN MV? {can_mv}, mvs = {mvs}")
+            self.shift_rings(0,1,mvs)
 
-        # can move
-        if e > -1:
-            # shift rings in the way
-            #       s_>e
-            # PRE   @OO.
-            # POST  .@OO
-            # shifting north
-            srs = set([(xr,c) for c in range(c0,e+1)])
-            srings = srs&self.rings
-            # isolated shifted rings
-            self.rings -= srings
-            # now shift up srings
-            srings = [(r,c+1) for r,c in srings]
-            # now add back to rings
-            self.rings |= set(srings)
-            # now move xpos
-            self.xpos = (xr, xc+1)
     def mv_w(self):
         xr,xc = self.xpos
-        c0 = 0
-        c1 = xc-1
-        # find first rock between me and edge
-        cbox = c1
-        while cbox >= 0:
-            if (xr,cbox) in self.boxes:
-                break
-            cbox -= 1
-        c0 = cbox
-        cs = range(c1,c0, -1)
-        ri = xr
-        e = -1
-        for ci in cs:
-            # find open slot
-            is_box  = (ri,ci) in self.boxes
-            is_ring = (ri,ci) in self.rings
-            if not is_box and not is_ring:
-                # appears to be open
-                # move here
-                e = ci
-                break
-
-        # can move
-        if e > -1:
-            # shift rings in the way
-            #       e<-s
-            # PRE   .OO@
-            # POST  OO@
-            # shifting north
-            srs = set([(xr,c) for c in range(e,xc)])
-            srings = srs&self.rings
-            # isolated shifted rings
-            self.rings -= srings
-            # now shift up srings
-            srings = [(r,c-1) for r,c in srings]
-            # now add back to rings
-            self.rings |= set(srings)
-            # now move xpos
-            self.xpos = (xr, xc-1)
+        can_mv = self.can_mv((xr,xc),(xr,xc-1))
+        if can_mv:
+            mvs = set()
+            mvs = self.collect_moves((xr,xc), (xr,xc-1), mvs)
+            #print(f"CAN MV? {can_mv}, mvs = {mvs}")
+            self.shift_rings(0,-1, mvs)
 
 
     def move(self, dir):
@@ -251,17 +250,22 @@ class Grid:
                 rc = (r,c)
                 if rc in self.rings:
                     v = "O"
+                elif rc in self.rrings:
+                    v = "]"
+                elif rc in self.lrings:
+                    v = "["
                 elif rc == self.xpos:
                     v = "@"
                 else:
                     v = self.G[r][c]
-                rstr += f"{v} "
+                #rstr += f"{v} "
+                rstr += f"{v}"
             rstr += "\n"
         return rstr
 
 
 
-def read_file(fname):
+def read_file(fname, is_p2=False):
 
     with open(fname, "r") as f:
         lines = f.readlines()
@@ -278,6 +282,24 @@ def read_file(fname):
             continue
         if reading_grid:
             chars = [c for c in line]
+            if is_p2:
+                wide_chars = []
+                for c in chars:
+                    if c == "#":
+                        wide_chars.append("#")
+                        wide_chars.append("#")
+                    elif c == "O":
+                        wide_chars.append("[")
+                        wide_chars.append("]")
+                    elif c == ".":
+                        wide_chars.append(".")
+                        wide_chars.append(".")
+                    elif c == "@":
+                        wide_chars.append("@")
+                        wide_chars.append(".")
+                    else:
+                        assert False, "bad char type read"
+                chars = wide_chars
             G.append(chars)
         else:
             for c in line:
@@ -288,22 +310,49 @@ def read_file(fname):
 
     return g,ins
 
+def p1():
+    fname = "ex15l.txt"
+    fname = "ex15s.txt"
+    fname = "in15.txt"
 
+    g,ins = read_file(fname)
+
+    #print(g)
+
+    i = ins.pop()
+
+    while i:
+        #print(f" i = {i}")
+        #x = input()
+        g.move(i)
+        #g.print()
+        i = ins.pop()
+
+    g.print()
+
+    ans = 0
+    for ring in g.rings:
+        r,c = ring
+        ans += 100*r + c
+
+
+    print(f"ans = {ans}")
 
 #--------------------------------
-fname = "ex15l.txt"
 fname = "ex15s.txt"
+fname = "ex15l.txt"
 fname = "in15.txt"
 
-g,ins = read_file(fname)
+g,ins = read_file(fname, True)
 
-#print(g)
+print(g)
 
 i = ins.pop()
 
 while i:
     #print(f" i = {i}")
     #x = input()
+    #sleep(1/5)
     g.move(i)
     #g.print()
     i = ins.pop()
@@ -311,10 +360,13 @@ while i:
 g.print()
 
 ans = 0
-for ring in g.rings:
+for ring in g.lrings:
     r,c = ring
     ans += 100*r + c
 
 
 print(f"ans = {ans}")
 
+
+"""
+"""
